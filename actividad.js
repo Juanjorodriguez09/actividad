e7bfa76eba43d5c45489185757a7f3e40f59dac3
Juanjorodriguez09 <140571811+Juanjorodriguez09@users.noreply.github.com>
2023-08-22 18:53:27 -0500

    actividad.js

--- actividad.js
+++ actividad.js
@@ -1,13 +1,8 @@
-// let i = 10
-// let f = 3
-// while(i>= f){
-//     console.log(i)
-// i--
-// }
+let i = 10
+let f = 3
+while(i>= f){
+    console.log(i)
+i--
+}
 
-let mensaje1 ='hola, como estan?'
-let mensaje2 = 'saludos'
-let x = 2 
-let y = 3
-let z = 2
-x == z && y > x ? console.log(mensaje1) : console.log(mensaje2)
+
